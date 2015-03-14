038b1edded0057984ea4230f489b597cfcc568f8
Christian Fei <crifei93@gmail.com>
2015-03-14 20:01:58 +0100

    init bower

--- src/cf-socket-io.js
+++ src/cf-socket-io.js
@@ -12,3 +12,33 @@ angular.module('cf-socket-io', ['cf-socket-io.utils'])
 .service('SocketIO', function(){
   return window.io || {connect:angular.noop}
 })
+
+
+angular.module('cf-socket-io.utils',[])
+.service('SocketIOUtils', function($timeout){
+  return {
+    decorate: decorate,
+    digestifyFn: digestifyFn
+  }
+  function decorate(obj){
+    console.log( '-- SocketIOUtils:decorate' )
+    for(var prop in obj){
+      console.log( '-- SocketIOUtils:decorate', prop )
+      if( obj[prop] instanceof Function ){
+        digestifyFn(obj, prop)
+      }
+    }
+  }
+
+  function digestifyFn(obj, prop){
+    var ref = obj[prop]
+    obj[prop] = function(){
+      var self = this
+      var args = Array.prototype.slice.apply(arguments)
+      $timeout(function(){
+        ref.apply(self, args)
+      })
+      return self
+    }
+  }
+})
