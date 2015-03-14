5fdece3d2515df1f65a63dd964cc4184c981fe27
Christian Fei <crifei93@gmail.com>
2015-03-14 18:03:21 +0100

    added karma

---
+++ karma.conf.js
@@ -0,0 +1,29 @@
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+
+    frameworks: ['jasmine'],
+
+    files: [
+      'src/**/*.js'
+    ],
+
+    exclude: [],
+
+    preprocessors: {},
+
+    reporters: ['progress'],
+
+    port: 9876,
+
+    colors: true,
+
+    logLevel: config.LOG_INFO,
+
+    autoWatch: false,
+
+    browsers: ['PhantomJS'],
+
+    singleRun: false
+  })
+}
