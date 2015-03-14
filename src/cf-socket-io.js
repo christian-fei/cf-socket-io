7e252dd29d0682b54564a32e473e076bada581fc
Christian Fei <crifei93@gmail.com>
2015-03-14 19:16:29 +0100

    removeListener when scope gets destroyed

--- src/cf-socket-io.js
+++ src/cf-socket-io.js
@@ -1,8 +1,11 @@
 angular.module('cf-socket-io', ['cf-socket-io.utils'])
 .factory('Socket', function(SocketIO, SocketIOUtils, $timeout, $rootScope){
-  return function Socket(path){
+  return function Socket(path, scope){
     var socket = SocketIO.connect(path)
     SocketIOUtils.decorate(socket)
+    if( scope ){
+      scope.$on('$destroy', socket.removeListener.bind(socket))
+    }
     return socket
   }
 })
