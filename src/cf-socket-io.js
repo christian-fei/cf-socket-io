8f043e6210b4134ba142d6f571adb48947417327
Christian Fei <crifei93@gmail.com>
2015-05-09 15:47:51 +0200

    updated api to take options

--- src/cf-socket-io.js
+++ src/cf-socket-io.js
@@ -1,10 +1,12 @@
 angular.module('cf-socket-io', ['cf-socket-io.utils'])
 .factory('Socket', function(SocketIO, SocketIOUtils, $timeout, $rootScope){
-  return function Socket(path, scope){
-    var socket = SocketIO.connect(path)
+  return function Socket(path, options){
+    var socket = SocketIO.connect(path, options || {})
     SocketIOUtils.decorate(socket)
-    if( scope ){
-      scope.$on('$destroy', socket.removeListener.bind(socket))
+    if( options ){
+      if( options.scope ){
+        options.scope.$on('$destroy', socket.removeListener.bind(socket))
+      }
     }
     return socket
   }
